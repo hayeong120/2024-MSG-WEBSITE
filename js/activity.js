e5c70450ffa2fcaec38ee1207edbf092a45b9d12
mic050r <s2216@e-mirim.hs.kr>
2024-03-12 01:26:06 +0900

    🥅 [fix] 활동페이지 에러 다시 수정

--- js/activity.js
+++ js/activity.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
     slidesPerView: 3,
     spaceBetween: 40,
     centeredSlides: true,
-    loop: false,
+    loop: true,
     autoplay: {
       delay: 5000,
     },
